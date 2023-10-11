5387115131a51a56c295a1817d82fc7b647891e9
Whitney-XU <bhxu0530@gmail.com>
2023-10-11 16:38:51 -0700

    Initial Commit

---
+++ CMU-X/CMU_XApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMU_XApp.swift
+//  CMU-X
+//
+//  Created by Binhui Xu on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CMU_XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
