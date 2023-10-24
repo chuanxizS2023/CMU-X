b55997a0bafd5812fe39a3b33ef56a381b72eda7
Chuanxin Zhang <chuanxiz@andrew.cmu.edu>
2023-10-24 14:41:25 -0700

    set up docker/sql connection

--- backend/postservice/src/main/java/com/cmux/postservice/repository/CommunityPostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cmux.postservice.repository;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cmux.postservice.model.CommunityPost;
-
-public interface CommunityPostRepository extends JpaRepository<CommunityPost, Long> {
-    CommunityPost findByCommunityPostid(long communityPostid);
-}
