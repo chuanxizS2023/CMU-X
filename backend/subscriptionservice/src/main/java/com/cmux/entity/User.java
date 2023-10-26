e806e0f0573c7a2cdb6b314f65532a3aaf1a0e24
zheruixS2023 <zheruix@andrew.cmu.edu>
2023-10-26 14:28:38 -0700

    add docker files

--- backend/subscriptionservice/src/main/java/com/cmux/entity/User.java
+++ backend/subscriptionservice/src/main/java/com/cmux/entity/User.java
@@ -4,7 +4,7 @@ import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 
-@Node(labels = {"Student"})
+@Node(labels = {"User"})
 public class User {
 
 	@Id
