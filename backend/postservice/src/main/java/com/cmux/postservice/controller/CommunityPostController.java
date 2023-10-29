72a9c8e63b64c3005c0ecd02ed0e798a19356d22
Chuanxin Zhang <chuanxiz@andrew.cmu.edu>
2023-10-29 12:28:16 -0700

    remove security and add logger

--- backend/postservice/src/main/java/com/cmux/postservice/controller/CommunityPostController.java
+++ backend/postservice/src/main/java/com/cmux/postservice/controller/CommunityPostController.java
@@ -2,9 +2,12 @@ package com.cmux.postservice.controller;
 
 import com.cmux.postservice.model.CommunityPost;
 import com.cmux.postservice.service.CommunityPostService;
-
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/community")
@@ -12,6 +15,9 @@ public class CommunityPostController {
     
     @Autowired
     private CommunityPostService communityPostService;
+
+    private static final Logger logger = LoggerFactory.getLogger(CommunityPostController.class);
+
 
     @GetMapping("/communitypost")
     public String getCommunityPost() {
@@ -32,5 +38,11 @@ public class CommunityPostController {
     @GetMapping("/{id}")
     public Optional<CommunityPost> getPostById(@PathVariable long communityPostid){
         return communityPostService.getPostById(communityPostid);
+    }
+
+    public Optional<CommunityPost> getPost(@PathVariable Long id) {
+        Optional<CommunityPost> post = communityPostService.getPostById(id);
+        logger.info("Retrieved post: " + post);
+        return post;
     }
 }
