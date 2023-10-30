a2207948d0ae7dd6fe89c0e236968144fda60679
Chuanxin Zhang <chuanxiz@andrew.cmu.edu>
2023-10-30 01:03:10 -0700

    add comment table template

---
+++ backend/postservice/src/main/java/com/cmux/postservice/model/Comment.java
@@ -0,0 +1,25 @@
+package com.cmux.postservice.model;
+
+import lombok.Data;
+import java.util.Date;
+import jakarta.persistence.*;
+
+
+@Data
+@Entity
+@Table(name = "comment")
+public class Comment {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long commentid;
+
+    private String content;
+    private Date created;
+    private String author_id;
+    private long likes;
+    
+    @ManyToOne
+    @JoinColumn(name = "communityPostid")
+    private CommunityPost post;
+}
---
+++ backend/postservice/src/main/java/com/cmux/postservice/service/CommentService.java
@@ -0,0 +1,21 @@
+package com.cmux.postservice.service;
+
+import org.springframework.stereotype.Service;
+import com.cmux.postservice.model.Comment;
+import com.cmux.postservice.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
+public class CommentService {
+    
+    @Autowired
+    private CommentRepository commentRepository;
+
+    public Comment saveComment(Comment comment){
+        return commentRepository.save(comment);
+    }
+
+    public Comment getCommentById(long commentId){
+        return commentRepository.findById(commentId).orElse(null);
+    }
+}
