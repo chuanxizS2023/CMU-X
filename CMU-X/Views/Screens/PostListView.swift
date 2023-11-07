d773800dc205d374fa7b328cf816c29e0f2974a8
Chuanxin Zhang <chuanxiz@andrew.cmu.edu>
2023-11-07 15:13:58 -0800

    Update PostListView.swift

--- CMU-X/Views/Screens/PostListView.swift
+++ CMU-X/Views/Screens/PostListView.swift
@@ -1,10 +1,7 @@
 import SwiftUI
 
 struct PostListView: View {
-    @State var posts = [
-        Post(username: "Harris Potter", content: "I love Professor Cecile and her classes...", timestamp: "14h"),Post(username: "Tom Dai", content: "Where is Hakan", timestamp: "14min")
-        // Add more dummy `Post` data as needed for the preview
-    ]
+    @State var posts: [Post] = [] // Assuming Post is a model conforming to Identifiable
 
     var body: some View {
         NavigationView {
@@ -12,13 +9,16 @@ struct PostListView: View {
                 PostView(post: post)
             }
             .navigationTitle("Community Posts")
-            .listStyle(PlainListStyle()) // Use `PlainListStyle` for a cleaner look
         }
     }
 }
 
 struct PostListView_Previews: PreviewProvider {
     static var previews: some View {
-        PostListView()
+        // Generate some example data for the preview
+        PostListView(posts: [
+            Post(id: "1", username: "User1", content: "Great course on system design!", timestamp: "3h ago", likes: 120, comments: 45),
+            Post(id: "2", username: "User2", content: "Loved the latest update!", timestamp: "1h ago", likes: 85, comments: 30)
+        ])
     }
 }
