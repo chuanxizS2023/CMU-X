c178bc8626a15d39341bb487968d553eef7f979f
Chuanxin Zhang <chuanxiz@andrew.cmu.edu>
2023-11-18 10:49:13 -0800

    docker-compose with frontend

---
+++ frontend/cmu-x-frontend-app/src/apis/communitypostAPIs/postAPI.js
@@ -0,0 +1,73 @@
+// src/api.js
+const baseUrl = 'http://localhost:9000/community';
+
+const headers = {
+  'Content-Type': 'application/json',
+};
+
+// Create a new post
+export const createPost = async (postData) => {
+  const response = await fetch(`${baseUrl}`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(postData),
+  });
+  return response.json();
+};
+
+// Add a like to a post
+export const addLike = async (postId) => {
+  const response = await fetch(`${baseUrl}/likes/${postId}`, {
+    method: 'POST',
+    headers,
+  });
+  return response.json();
+};
+
+// Get a post by ID
+export const getPostById = async (postId) => {
+  const response = await fetch(`${baseUrl}/${postId}`, {
+    method: 'GET',
+    headers,
+  });
+  return response.json();
+};
+
+// Delete a post by ID
+export const deletePost = async (postId) => {
+  const response = await fetch(`${baseUrl}/${postId}`, {
+    method: 'DELETE',
+    headers,
+  });
+  return response.json();
+};
+
+// Update a post by ID
+export const updatePost = async (postId, updateData) => {
+  const response = await fetch(`${baseUrl}/${postId}`, {
+    method: 'PUT',
+    headers,
+    body: JSON.stringify(updateData),
+  });
+  return response.json();
+};
+
+// Mark a post as looking for teammates
+export const markAsFindTeammatePost = async (postId, updateData) => {
+  const response = await fetch(`${baseUrl}/find-teammate/${postId}`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(updateData),
+  });
+  return response.json();
+};
+
+// Add a team member to a post
+export const addTeamMembers = async (postId, username) => {
+  const response = await fetch(`${baseUrl}/${postId}/team-members`, {
+    method: 'PUT',
+    headers,
+    body: JSON.stringify({ username }),
+  });
+  return response.json();
+};
--- frontend/cmu-x-frontend-app/src/pages/communityPost/communityPostPage.js
+++ frontend/cmu-x-frontend-app/src/pages/communityPost/communityPostPage.js
@@ -1,8 +1,26 @@
-import React from 'react';
+// src/pages/CommunityPage.js
+import React, { useState, useEffect } from 'react';
 import Header from '../../components/common/header';
 import PostList from '../../containers/communityPost/postContainer';
+import { getPostById } from '../../apis/communitypostAPIs/postAPI'; // Adjust the import path as necessary
 
-const CommunityPage = ({ posts }) => {
+const CommunityPage = () => {
+  const [post, setPost] = useState(null);
+  const postId = 1; // Replace with the actual post ID you want to fetch
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const data = await getPostById(postId);
+        setPost(data);
+      } catch (error) {
+        console.error('Failed to fetch post:', error);
+      }
+    };
+
+    fetchPost();
+  }, [postId]);
+
   const styles = {
     page: {
       maxWidth: '600px',
@@ -13,7 +31,8 @@ const CommunityPage = ({ posts }) => {
   return (
     <div style={styles.page}>
       <Header title="CMU-X Community" />
-      <PostList posts={posts} />
+      {post && <PostList posts={[post]} />}
+      <div>Hello world</div>
     </div>
   );
 };
