02788ba4b5935682c9df30447242ed554d84f709
Chuanxin Zhang <chuanxiz@andrew.cmu.edu>
2023-11-19 22:32:57 -0800

    allow CORs and add mem limit for docker services

--- backend/postservice/src/main/java/com/cmux/postservice/controller/CommentController.java
+++ backend/postservice/src/main/java/com/cmux/postservice/controller/CommentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/comments")
 
 public class CommentController {
