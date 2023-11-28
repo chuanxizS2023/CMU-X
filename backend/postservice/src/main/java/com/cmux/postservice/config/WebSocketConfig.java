d0eea0e8d963b460b0db72803c82a4d7715337a9
Chuanxin Zhang <chuanxiz@andrew.cmu.edu>
2023-11-28 13:14:54 -0800

    setup rabbitmq with stomp enable

--- backend/postservice/src/main/java/com/cmux/postservice/config/WebSocketConfig.java
+++ backend/postservice/src/main/java/com/cmux/postservice/config/WebSocketConfig.java
@@ -12,13 +12,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableStompBrokerRelay("/topic", "/queue")
+        config
+            .setApplicationDestinationPrefixes("/app")
+            .enableStompBrokerRelay("/topic", "/queue")
             .setRelayHost("rabbitmq")
             .setRelayPort(61613)
             .setClientLogin("guest")
             .setClientPasscode("guest");
-
-        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
