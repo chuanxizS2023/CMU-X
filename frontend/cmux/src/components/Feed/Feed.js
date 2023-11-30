609ddd196dbc741eb6bf7848f03067f9c47b4a02
zheruixS2023 <zheruix@andrew.cmu.edu>
2023-11-29 16:24:58 -0800

    Add fancy frontend

---
+++ frontend/cmux/src/components/Feed/Feed.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Feed.css";
+import TweetBox from "./TweetBox/TweetBox";
+import Post from "./Post/Post";
+import HomeStars from "../icons/HomeStars";
+import BottomSidebar from "../BottomSidebar/BottomSidebar";
+import { useSelector } from "react-redux";
+import { Avatar } from "@material-ui/core";
+import DrawerBar from "../DrawerBar/DrawerBar";
+import Loading from "../Loading/Loading";
+import logo from "../../assets/cmux_logo_no_bg.png";
+
+function Feed() {
+  const { posts } = useSelector((state) => state).posts;
+  const [isDrawerBar, setIsDrawerBar] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  setTimeout(() => {
+    setLoading(false);
+  }, 500);
+  return (
+    <section className="feed">
+      {isDrawerBar && (
+        <div onClick={() => setIsDrawerBar(false)} className="drawerBarPanel" />
+      )}
+      <DrawerBar active={isDrawerBar} />
+      <div className="feed-header">
+        <div onClick={() => setIsDrawerBar(true)}>
+          <Avatar src={logo} />
+        </div>
+        <div className="feed-headerText">
+          <span>Home</span>
+        </div>
+        <div className="homeStarsCol">
+          <HomeStars className="homeStars" width={22} height={22} />
+        </div>
+      </div>
+      <TweetBox />
+      {loading ? (
+        <Loading />
+      ) : (
+        <article>
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              username={post.username}
+              userimage={post.userimage}
+              date={post.date}
+              displayName={post.displayName}
+              text={post.text}
+              shareImage={post.shareImage}
+            />
+          ))}
+        </article>
+      )}
+      <BottomSidebar />
+    </section>
+  );
+}
+
+export default Feed;
