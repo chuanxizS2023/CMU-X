9e3fa060ab65b49f3c122a1081edd8e22624fe05
zheruixS2023 <zheruix@andrew.cmu.edu>
2023-12-05 14:07:03 -0800

    Add MQ

---
+++ backend/subscriptionservice/src/main/java/com/cmux/controller/MQProducer.java
@@ -0,0 +1,25 @@
+package com.cmux.controller;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
+@Service
+public class MQProducer {
+    
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.user.routing.key}")
+    private String userRoutingKey;
+
+    public MQProducer (RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendImageToUser(String message) {
+        rabbitTemplate.convertAndSend(exchange, userRoutingKey, message);
+    }
+}
